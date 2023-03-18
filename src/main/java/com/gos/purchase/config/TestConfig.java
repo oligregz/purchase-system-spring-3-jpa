afbf865f3b492203cec2bd10324ca925d76d3e80
oligregz <gregorysak90@gmail.com>
2023-03-18 17:01:00 -0300

    feat: seeder and json response handling implemented

--- src/main/java/com/gos/purchase/config/TestConfig.java
+++ src/main/java/com/gos/purchase/config/TestConfig.java
@@ -1,11 +1,14 @@
 package com.gos.purchase.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import com.gos.purchase.entities.Order;
 import com.gos.purchase.entities.User;
+import com.gos.purchase.repositories.OrderRepositry;
 import com.gos.purchase.repositories.UserRepositry;
 
 /**
@@ -20,6 +23,9 @@ public class TestConfig implements CommandLineRunner{
   @Autowired
   private UserRepositry userRepository;
 
+  @Autowired
+  private OrderRepositry orderRepository;
+
   @Override
   public void run(String... args) throws Exception {
     
@@ -28,7 +34,12 @@ public class TestConfig implements CommandLineRunner{
     User usr2 = new User(null, 
         "Eduardo Trinca Ferro", "trincaferro@gmail.com", "+55 12 988689402", "#Tr1nK4F3rr0");
 
+    Order odr1 = new Order(null, Instant.parse("2019-06-11T19:53:07Z"), usr1);
+    Order odr2 = new Order(null, Instant.parse("2019-03-24T13:42:12Z"), usr2);
+    Order odr3 = new Order(null, Instant.parse("2019-05-03T03:20:32Z"), usr1);
+
     userRepository.saveAll(Arrays.asList(usr1, usr2));
+    orderRepository.saveAll(Arrays.asList(odr1, odr2, odr3));
   }
 
   
--- src/main/java/com/gos/purchase/entities/Order.java
+++ src/main/java/com/gos/purchase/entities/Order.java
@@ -1,5 +1,9 @@
 package com.gos.purchase.entities;
 
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,9 +11,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.io.Serializable;
-import java.time.Instant;
-import java.util.Objects;
 
 /**
  * Order Model.
@@ -24,6 +25,8 @@ public class Order implements Serializable {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @JsonFormat(shape = JsonFormat.Shape.STRING,
+      pattern = "yyyy-MM-dd'T'HH:mm:ss'Z", timezone = "GMT")
   private Instant moment;
 
   @ManyToOne
