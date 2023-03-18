a2e9229a16cf1355bef4d0012c7505e9fb4e2186
oligregz <gregorysak90@gmail.com>
2023-03-18 10:09:51 -0300

    [Purchase System] - Starting

---
+++ src/main/java/com/gos/purchase/PurchaseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gos.purchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gos/purchase/PurchaseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gos.purchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
