1181099707614ae3c73eba0f891d13ea28197897
oligregz <gregorysak90@gmail.com>
2023-03-18 10:24:36 -0300

    feat: hashCode and equals implemented

--- src/main/java/com/gos/purchase/entities/User.java
+++ src/main/java/com/gos/purchase/entities/User.java
@@ -1,5 +1,7 @@
 package com.gos.purchase.entities;
 
+import java.util.Objects;
+
 public class User {
 
   private Long id;
@@ -62,6 +64,23 @@ public class User {
 
   public void setPassword(String password) {
     this.password = password;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    User other = (User) obj;
+    return Objects.equals(id, other.id);
   }
 
 }
