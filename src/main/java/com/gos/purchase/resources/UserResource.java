c9a71c8fcf27242ad1cc459f2e901a94ef491bb3
oligregz <gregorysak90@gmail.com>
2023-03-18 15:01:45 -0300

    feat: find by id user method imlemented

--- src/main/java/com/gos/purchase/resources/UserResource.java
+++ src/main/java/com/gos/purchase/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.gos.purchase.entities.User;
@@ -20,11 +21,18 @@ public class UserResource {
   private UserService service;
 
   /**
-   * Test method.
+   * Find all method.
    */
   @GetMapping
   public ResponseEntity<List<User>> findAll() {
     List<User> list = service.findAll();
     return ResponseEntity.ok().body(list);
   }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<User> findById(@PathVariable Long id) {
+    User obj = service.findById(id);
+    return ResponseEntity.ok().body(obj);
+  }
+  
 }
--- src/main/java/com/gos/purchase/services/UserService.java
+++ src/main/java/com/gos/purchase/services/UserService.java
@@ -1,10 +1,11 @@
 package com.gos.purchase.services;
 
-import com.gos.purchase.entities.User;
-import com.gos.purchase.repositories.UserRepositry;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.gos.purchase.entities.User;
+import com.gos.purchase.repositories.UserRepositry;
 
 /**
  * Service.
@@ -18,5 +19,10 @@ public class UserService {
 
   public List<User> findAll() {
     return repository.findAll();
+  }
+  
+  public User findById(Long id) {
+    Optional<User> obj = repository.findById(id);
+    return obj.get();
   }
 }
