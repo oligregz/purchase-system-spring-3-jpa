ca23b87312c56dad3390c44c86b7dba297166285
oligregz <gregorysak90@gmail.com>
2023-03-18 16:32:20 -0300

    feat: order controller implemented

---
+++ src/main/java/com/gos/purchase/resources/OrderResource.java
@@ -0,0 +1,38 @@
+package com.gos.purchase.resources;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.gos.purchase.entities.Order;
+import com.gos.purchase.services.OrderService;
+
+/**
+ * Order controller.
+ */
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+
+  @Autowired
+  private OrderService service;
+
+  /**
+   * Find all method.
+   */
+  @GetMapping
+  public ResponseEntity<List<Order>> findAll() {
+    List<Order> list = service.findAll();
+    return ResponseEntity.ok().body(list);
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<Order> findById(@PathVariable Long id) {
+    Order obj = service.findById(id);
+    return ResponseEntity.ok().body(obj);
+  }
+  
+}
--- src/main/java/com/gos/purchase/services/OrderService.java
+++ src/main/java/com/gos/purchase/services/OrderService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.gos.purchase.entities.Order;
-import com.gos.purchase.repositories.UserRepositry;
+import com.gos.purchase.repositories.OrderRepositry;
 
 /**
  * Service.
